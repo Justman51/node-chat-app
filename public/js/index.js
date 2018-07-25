46395a0449f0a5de300dafd72ac6ea2f130fbdbb
Jeffrey Ololu <jeffreyololu@Jeffreys-iMac.local>
2018-07-25 08:19:47 +0200

    Added broadcasting event emiter to de server

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io(); // Making a request to the server
+socket.on('connect',  function() {
+    console.log('Connected to server');
+
+    // socket.emit('createMessage', {
+    //     from: 'jen@example.com',
+    //     text: 'Hey, this is Andrew.'
+    // })
+});
+
+socket.on('newMessage', function(message) { // receiver is here
+    console.log('Got your message', message);
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
--- server/server.js
+++ server/server.js
@@ -12,6 +12,21 @@ app.use(express.static(path.join(__dirname, '../public')))
 io.on('connection', (socket) =>{ 
     console.log('New user connected');
 
+    // socket.emit('newMessage', { 
+    //     from: 'joker@example.com',
+    //     text: 'Hey, what going on',
+    //     createAt: 123
+    // });
+
+    socket.on('createMessage', (message)=> { // emit to only one user connected
+      // console.log('Got your message thanks', message);
+/* Broadcasting */  io.emit('newMessage', { // Emits to every user connected
+           from: message.from,
+           text: message.text,
+           createAt: new Date().getTime()
+       })
+    })
+
     socket.on('disconnect', (reason) => {
         console.log('User was disconnected');
       });
